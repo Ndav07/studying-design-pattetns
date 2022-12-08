6061050f39134fcf5b3a8e9eb357b8c3fd376f0a
Níkollas <nikollasdavidor@hotmail.com>
2022-12-08 12:28:30 -0300

    adpter

---
+++ test/structural/adapter/Transaction.spec.ts
@@ -0,0 +1,35 @@
+// CONVERTER a interface de uma classe em outra interface, esperada pelos cliente 
+// O Adpter permite que classes com interfaces incompatíveis trabalhes em conjunto
+
+import PayPalTransaction from "../../../src/structural/adpter/PayPal-transaction"
+import PayPalTransactionAdapter from "../../../src/structural/adpter/PayPal-transaction-adpter"
+import StripeTransaction from "../../../src/structural/adpter/Stripe-transaction"
+import StripeTransactionAdapter from "../../../src/structural/adpter/Stripe-transaction-adpter"
+
+describe('Adapter, test transaction', () => {
+  it('should create a Stripe transaction', () => {
+    const stripeTransaction = new StripeTransaction('AHN786AB8', 1000, 2)
+    expect(stripeTransaction.code).toBe('AHN786AB8')
+  })
+
+  it('should create a PayPal transaction', () => {
+    const paypalTransaction = new PayPalTransaction(8756464, 1000, 'F')
+    expect(paypalTransaction.id).toBe(8756464)
+  })
+
+  it('should create transaction from the Stripe', () => {
+    const stripeTransaction = new StripeTransaction('AHN786AB8', 1000, 2)
+    const transaction = new StripeTransactionAdapter(stripeTransaction)
+    expect(transaction.trackNumber).toBe('AHN786AB8')
+    expect(transaction.amount).toBe(1000)
+    expect(transaction.status).toBe('paid')
+  })
+
+  it('should create transaction from the PayPal', () => {
+    const paypalTransaction = new PayPalTransaction(435646478, 1000, 'P')
+    const transaction = new PayPalTransactionAdapter(paypalTransaction)
+    expect(transaction.trackNumber).toBe('435646478')
+    expect(transaction.amount).toBe(1000)
+    expect(transaction.status).toBe('waiting_payment')
+  })
+})
